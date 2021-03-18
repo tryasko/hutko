4be9a84d042e4a0a5604e0a2d707535e4c87f0f7
tryasko <tryasko@gmail.com>
2021-03-18 22:06:38 +0200

    Use prettier

--- benchmarks/commonUtilities.js
+++ benchmarks/commonUtilities.js
@@ -2,13 +2,12 @@
  * Functions that are used in several benchmark tests
  */
 
-var customUtils = require('../lib/customUtils')
-  , fs = require('fs')
-  , path = require('path')
-  , Datastore = require('../lib/datastore')
-  , Persistence = require('../lib/persistence')
-  , executeAsap   // process.nextTick or setImmediate depending on your Node version
-  ;
+var customUtils = require("../lib/customUtils"),
+  fs = require("fs"),
+  path = require("path"),
+  Datastore = require("../lib/datastore"),
+  Persistence = require("../lib/persistence"),
+  executeAsap; // process.nextTick or setImmediate depending on your Node version
 
 try {
   executeAsap = setImmediate;
@@ -16,19 +15,21 @@ try {
   executeAsap = process.nextTick;
 }
 
-
 /**
  * Configure the benchmark
  */
 module.exports.getConfiguration = function (benchDb) {
-  var d, n
-    , program = require('commander')
-    ;
-
+  var d,
+    n,
+    program = require("commander");
   program
-    .option('-n --number [number]', 'Size of the collection to test on', parseInt)
-    .option('-i --with-index', 'Use an index')
-    .option('-m --in-memory', 'Test with an in-memory only store')
+    .option(
+      "-n --number [number]",
+      "Size of the collection to test on",
+      parseInt
+    )
+    .option("-i --with-index", "Use an index")
+    .option("-m --in-memory", "Test with an in-memory only store")
     .parse(process.argv);
 
   n = program.number || 10000;
@@ -36,16 +37,17 @@ module.exports.getConfiguration = function (benchDb) {
   console.log("----------------------------");
   console.log("Test with " + n + " documents");
   console.log(program.withIndex ? "Use an index" : "Don't use an index");
-  console.log(program.inMemory ? "Use an in-memory datastore" : "Use a persistent datastore");
+  console.log(
+    program.inMemory
+      ? "Use an in-memory datastore"
+      : "Use a persistent datastore"
+  );
   console.log("----------------------------");
 
-  d = new Datastore({ filename: benchDb
-                    , inMemoryOnly: program.inMemory
-                    });
+  d = new Datastore({ filename: benchDb, inMemoryOnly: program.inMemory });
 
   return { n: n, d: d, program: program };
 };
-
 
 /**
  * Ensure the workspace exists and the db datafile is empty
@@ -55,23 +57,27 @@ module.exports.prepareDb = function (filename, cb) {
     fs.exists(filename, function (exists) {
       if (exists) {
         fs.unlink(filename, cb);
-      } else { return cb(); }
+      } else {
+        return cb();
+      }
     });
   });
 };
-
 
 /**
  * Return an array with the numbers from 0 to n-1, in a random order
  * Uses Fisher Yates algorithm
  * Useful to get fair tests
  */
-function getRandomArray (n) {
-  var res = []
-    , i, j, temp
-    ;
+function getRandomArray(n) {
+  var res = [],
+    i,
+    j,
+    temp;
 
-  for (i = 0; i < n; i += 1) { res[i] = i; }
+  for (i = 0; i < n; i += 1) {
+    res[i] = i;
+  }
 
   for (i = n - 1; i >= 1; i -= 1) {
     j = Math.floor((i + 1) * Math.random());
@@ -81,25 +87,25 @@ function getRandomArray (n) {
   }
 
   return res;
-};
+}
 module.exports.getRandomArray = getRandomArray;
-
 
 /**
  * Insert a certain number of documents for testing
  */
 module.exports.insertDocs = function (d, n, profiler, cb) {
-  var beg = new Date()
-    , order = getRandomArray(n)
-    ;
-
-  profiler.step('Begin inserting ' + n + ' docs');
+  var beg = new Date(),
+    order = getRandomArray(n);
+  profiler.step("Begin inserting " + n + " docs");
 
   function runFrom(i) {
-    if (i === n) {   // Finished
-      var opsPerSecond = Math.floor(1000* n / profiler.elapsedSinceLastStep());
+    if (i === n) {
+      // Finished
+      var opsPerSecond = Math.floor(
+        (1000 * n) / profiler.elapsedSinceLastStep()
+      );
       console.log("===== RESULT (insert) ===== " + opsPerSecond + " ops/s");
-      profiler.step('Finished inserting ' + n + ' docs');
+      profiler.step("Finished inserting " + n + " docs");
       profiler.insertOpsPerSecond = opsPerSecond;
       return cb();
     }
@@ -113,26 +119,30 @@ module.exports.insertDocs = function (d, n, profiler, cb) {
   runFrom(0);
 };
 
-
 /**
  * Find documents with find
  */
 module.exports.findDocs = function (d, n, profiler, cb) {
-  var beg = new Date()
-    , order = getRandomArray(n)
-    ;
-
+  var beg = new Date(),
+    order = getRandomArray(n);
   profiler.step("Finding " + n + " documents");
 
   function runFrom(i) {
-    if (i === n) {   // Finished
-      console.log("===== RESULT (find) ===== " + Math.floor(1000* n / profiler.elapsedSinceLastStep()) + " ops/s");
-      profiler.step('Finished finding ' + n + ' docs');
+    if (i === n) {
+      // Finished
+      console.log(
+        "===== RESULT (find) ===== " +
+          Math.floor((1000 * n) / profiler.elapsedSinceLastStep()) +
+          " ops/s"
+      );
+      profiler.step("Finished finding " + n + " docs");
       return cb();
     }
 
     d.find({ docNumber: order[i] }, function (err, docs) {
-      if (docs.length !== 1 || docs[0].docNumber !== order[i]) { return cb('One find didnt work'); }
+      if (docs.length !== 1 || docs[0].docNumber !== order[i]) {
+        return cb("One find didnt work");
+      }
       executeAsap(function () {
         runFrom(i + 1);
       });
@@ -141,17 +151,16 @@ module.exports.findDocs = function (d, n, profiler, cb) {
   runFrom(0);
 };
 
-
 /**
  * Find documents with find and the $in operator
  */
 module.exports.findDocsWithIn = function (d, n, profiler, cb) {
-  var beg = new Date()
-    , order = getRandomArray(n)
-    , ins = [], i, j
-    , arraySize = Math.min(10, n)   // The array for $in needs to be smaller than n (inclusive)
-    ;
-
+  var beg = new Date(),
+    order = getRandomArray(n),
+    ins = [],
+    i,
+    j,
+    arraySize = Math.min(10, n); // The array for $in needs to be smaller than n (inclusive)
   // Preparing all the $in arrays, will take some time
   for (i = 0; i < n; i += 1) {
     ins[i] = [];
@@ -164,14 +173,21 @@ module.exports.findDocsWithIn = function (d, n, profiler, cb) {
   profiler.step("Finding " + n + " documents WITH $IN OPERATOR");
 
   function runFrom(i) {
-    if (i === n) {   // Finished
-      console.log("===== RESULT (find with in selector) ===== " + Math.floor(1000* n / profiler.elapsedSinceLastStep()) + " ops/s");
-      profiler.step('Finished finding ' + n + ' docs');
+    if (i === n) {
+      // Finished
+      console.log(
+        "===== RESULT (find with in selector) ===== " +
+          Math.floor((1000 * n) / profiler.elapsedSinceLastStep()) +
+          " ops/s"
+      );
+      profiler.step("Finished finding " + n + " docs");
       return cb();
     }
 
     d.find({ docNumber: { $in: ins[i] } }, function (err, docs) {
-      if (docs.length !== arraySize) { return cb('One find didnt work'); }
+      if (docs.length !== arraySize) {
+        return cb("One find didnt work");
+      }
       executeAsap(function () {
         runFrom(i + 1);
       });
@@ -179,27 +195,31 @@ module.exports.findDocsWithIn = function (d, n, profiler, cb) {
   }
   runFrom(0);
 };
-
 
 /**
  * Find documents with findOne
  */
 module.exports.findOneDocs = function (d, n, profiler, cb) {
-  var beg = new Date()
-    , order = getRandomArray(n)
-    ;
-
+  var beg = new Date(),
+    order = getRandomArray(n);
   profiler.step("FindingOne " + n + " documents");
 
   function runFrom(i) {
-    if (i === n) {   // Finished
-      console.log("===== RESULT (findOne) ===== " + Math.floor(1000* n / profiler.elapsedSinceLastStep()) + " ops/s");
-      profiler.step('Finished finding ' + n + ' docs');
+    if (i === n) {
+      // Finished
+      console.log(
+        "===== RESULT (findOne) ===== " +
+          Math.floor((1000 * n) / profiler.elapsedSinceLastStep()) +
+          " ops/s"
+      );
+      profiler.step("Finished finding " + n + " docs");
       return cb();
     }
 
     d.findOne({ docNumber: order[i] }, function (err, doc) {
-      if (!doc || doc.docNumber !== order[i]) { return cb('One find didnt work'); }
+      if (!doc || doc.docNumber !== order[i]) {
+        return cb("One find didnt work");
+      }
       executeAsap(function () {
         runFrom(i + 1);
       });
@@ -207,66 +227,86 @@ module.exports.findOneDocs = function (d, n, profiler, cb) {
   }
   runFrom(0);
 };
-
 
 /**
  * Update documents
  * options is the same as the options object for update
  */
 module.exports.updateDocs = function (options, d, n, profiler, cb) {
-  var beg = new Date()
-    , order = getRandomArray(n)
-    ;
-
+  var beg = new Date(),
+    order = getRandomArray(n);
   profiler.step("Updating " + n + " documents");
 
   function runFrom(i) {
-    if (i === n) {   // Finished
-      console.log("===== RESULT (update) ===== " + Math.floor(1000* n / profiler.elapsedSinceLastStep()) + " ops/s");
-      profiler.step('Finished updating ' + n + ' docs');
+    if (i === n) {
+      // Finished
+      console.log(
+        "===== RESULT (update) ===== " +
+          Math.floor((1000 * n) / profiler.elapsedSinceLastStep()) +
+          " ops/s"
+      );
+      profiler.step("Finished updating " + n + " docs");
       return cb();
     }
 
     // Will not actually modify the document but will take the same time
-    d.update({ docNumber: order[i] }, { docNumber: order[i] }, options, function (err, nr) {
-      if (err) { return cb(err); }
-      if (nr !== 1) { return cb('One update didnt work'); }
-      executeAsap(function () {
-        runFrom(i + 1);
-      });
-    });
+    d.update(
+      { docNumber: order[i] },
+      { docNumber: order[i] },
+      options,
+      function (err, nr) {
+        if (err) {
+          return cb(err);
+        }
+        if (nr !== 1) {
+          return cb("One update didnt work");
+        }
+        executeAsap(function () {
+          runFrom(i + 1);
+        });
+      }
+    );
   }
   runFrom(0);
 };
-
 
 /**
  * Remove documents
  * options is the same as the options object for update
  */
 module.exports.removeDocs = function (options, d, n, profiler, cb) {
-  var beg = new Date()
-    , order = getRandomArray(n)
-    ;
-
+  var beg = new Date(),
+    order = getRandomArray(n);
   profiler.step("Removing " + n + " documents");
 
   function runFrom(i) {
-    if (i === n) {   // Finished
+    if (i === n) {
+      // Finished
       // opsPerSecond corresponds to 1 insert + 1 remove, needed to keep collection size at 10,000
       // We need to subtract the time taken by one insert to get the time actually taken by one remove
-      var opsPerSecond = Math.floor(1000 * n / profiler.elapsedSinceLastStep());
-      var removeOpsPerSecond = Math.floor(1 / ((1 / opsPerSecond) - (1 / profiler.insertOpsPerSecond)))
-      console.log("===== RESULT (remove) ===== " + removeOpsPerSecond + " ops/s");
-      profiler.step('Finished removing ' + n + ' docs');
+      var opsPerSecond = Math.floor(
+        (1000 * n) / profiler.elapsedSinceLastStep()
+      );
+      var removeOpsPerSecond = Math.floor(
+        1 / (1 / opsPerSecond - 1 / profiler.insertOpsPerSecond)
+      );
+      console.log(
+        "===== RESULT (remove) ===== " + removeOpsPerSecond + " ops/s"
+      );
+      profiler.step("Finished removing " + n + " docs");
       return cb();
     }
 
     d.remove({ docNumber: order[i] }, options, function (err, nr) {
-      if (err) { return cb(err); }
-      if (nr !== 1) { return cb('One remove didnt work'); }
-      d.insert({ docNumber: order[i] }, function (err) {   // We need to reinsert the doc so that we keep the collection's size at n
-                                                           // So actually we're calculating the average time taken by one insert + one remove
+      if (err) {
+        return cb(err);
+      }
+      if (nr !== 1) {
+        return cb("One remove didnt work");
+      }
+      d.insert({ docNumber: order[i] }, function (err) {
+        // We need to reinsert the doc so that we keep the collection's size at n
+        // So actually we're calculating the average time taken by one insert + one remove
         executeAsap(function () {
           runFrom(i + 1);
         });
@@ -276,21 +316,23 @@ module.exports.removeDocs = function (options, d, n, profiler, cb) {
   runFrom(0);
 };
 
-
 /**
  * Load database
  */
 module.exports.loadDatabase = function (d, n, profiler, cb) {
-  var beg = new Date()
-    , order = getRandomArray(n)
-    ;
-
+  var beg = new Date(),
+    order = getRandomArray(n);
   profiler.step("Loading the database " + n + " times");
 
   function runFrom(i) {
-    if (i === n) {   // Finished
-      console.log("===== RESULT ===== " + Math.floor(1000* n / profiler.elapsedSinceLastStep()) + " ops/s");
-      profiler.step('Finished loading a database' + n + ' times');
+    if (i === n) {
+      // Finished
+      console.log(
+        "===== RESULT ===== " +
+          Math.floor((1000 * n) / profiler.elapsedSinceLastStep()) +
+          " ops/s"
+      );
+      profiler.step("Finished loading a database" + n + " times");
       return cb();
     }
 
@@ -302,7 +344,3 @@ module.exports.loadDatabase = function (d, n, profiler, cb) {
   }
   runFrom(0);
 };
-
-
-
-
--- test/executor.test.js
+++ test/executor.test.js
@@ -1,45 +1,39 @@
-var should = require('chai').should()
-  , assert = require('chai').assert
-  , testDb = 'workspace/test.db'
-  , fs = require('fs')
-  , path = require('path')
-  , _ = require('underscore')
-  , async = require('async')
-  , model = require('../lib/model')
-  , Datastore = require('../lib/datastore')
-  , Persistence = require('../lib/persistence')
-  ;
-
-
+var assert = require("chai").assert,
+  testDb = "workspace/test.db",
+  fs = require("fs"),
+  path = require("path"),
+  async = require("async"),
+  Datastore = require("../lib/datastore"),
+  Persistence = require("../lib/persistence");
 // Test that even if a callback throws an exception, the next DB operations will still be executed
 // We prevent Mocha from catching the exception we throw on purpose by remembering all current handlers, remove them and register them back after test ends
-function testThrowInCallback (d, done) {
-  var currentUncaughtExceptionHandlers = process.listeners('uncaughtException');
+function testThrowInCallback(d, done) {
+  var currentUncaughtExceptionHandlers = process.listeners("uncaughtException");
 
-  process.removeAllListeners('uncaughtException');
+  process.removeAllListeners("uncaughtException");
 
-  process.on('uncaughtException', function (err) {
+  process.on("uncaughtException", function (err) {
     // Do nothing with the error which is only there to test we stay on track
   });
 
   d.find({}, function (err) {
     process.nextTick(function () {
       d.insert({ bar: 1 }, function (err) {
-        process.removeAllListeners('uncaughtException');
+        process.removeAllListeners("uncaughtException");
         for (var i = 0; i < currentUncaughtExceptionHandlers.length; i += 1) {
-          process.on('uncaughtException', currentUncaughtExceptionHandlers[i]);
+          process.on("uncaughtException", currentUncaughtExceptionHandlers[i]);
         }
 
         done();
       });
     });
 
-    throw new Error('Some error');
+    throw new Error("Some error");
   });
 }
 
 // Test that if the callback is falsy, the next DB operations will still be executed
-function testFalsyCallback (d, done) {
+function testFalsyCallback(d, done) {
   d.insert({ a: 1 }, null);
   process.nextTick(function () {
     d.update({ a: 1 }, { a: 2 }, {}, null);
@@ -60,12 +54,12 @@ function testFalsyCallback (d, done) {
 
 // Test that operations are executed in the right order
 // We prevent Mocha from catching the exception we throw on purpose by remembering all current handlers, remove them and register them back after test ends
-function testRightOrder (d, done) {
-  var currentUncaughtExceptionHandlers = process.listeners('uncaughtException');
+function testRightOrder(d, done) {
+  var currentUncaughtExceptionHandlers = process.listeners("uncaughtException");
 
-  process.removeAllListeners('uncaughtException');
+  process.removeAllListeners("uncaughtException");
 
-  process.on('uncaughtException', function (err) {
+  process.on("uncaughtException", function (err) {
     // Do nothing with the error which is only there to test we stay on track
   });
 
@@ -82,9 +76,16 @@ function testRightOrder (d, done) {
               d.find({}, function (err, docs) {
                 docs[0].a.should.equal(3);
 
-                process.removeAllListeners('uncaughtException');
-                for (var i = 0; i < currentUncaughtExceptionHandlers.length; i += 1) {
-                  process.on('uncaughtException', currentUncaughtExceptionHandlers[i]);
+                process.removeAllListeners("uncaughtException");
+                for (
+                  var i = 0;
+                  i < currentUncaughtExceptionHandlers.length;
+                  i += 1
+                ) {
+                  process.on(
+                    "uncaughtException",
+                    currentUncaughtExceptionHandlers[i]
+                  );
                 }
 
                 done();
@@ -92,7 +93,7 @@ function testRightOrder (d, done) {
             });
           });
 
-          throw new Error('Some error');
+          throw new Error("Some error");
         });
       });
     });
@@ -103,19 +104,18 @@ function testRightOrder (d, done) {
 // is meant to address the deprecation warning:
 // (node) warning: Recursive process.nextTick detected. This will break in the next version of node. Please use setImmediate for recursive deferral.
 // see
-var testEventLoopStarvation = function(d, done){
-   var times = 1001;
-   var i = 0;
-   while ( i <times) {
-      i++;
-     d.find({"bogus": "search"}, function (err, docs) {
-     });
-   }
-   done();
+var testEventLoopStarvation = function (d, done) {
+  var times = 1001;
+  var i = 0;
+  while (i < times) {
+    i++;
+    d.find({ bogus: "search" }, function (err, docs) {});
+  }
+  done();
 };
 
 // Test that operations are executed in the right order even with no callback
-function testExecutorWorksWithoutCallback (d, done) {
+function testExecutorWorksWithoutCallback(d, done) {
   d.insert({ a: 1 });
   d.insert({ a: 2 }, false);
   d.find({}, function (err, docs) {
@@ -124,10 +124,8 @@ function testExecutorWorksWithoutCallback (d, done) {
   });
 }
 
-
-describe('Executor', function () {
-
-  describe('With persistent database', function () {
+describe("Executor", function () {
+  describe("With persistent database", function () {
     var d;
 
     beforeEach(function (done) {
@@ -135,50 +133,56 @@ describe('Executor', function () {
       d.filename.should.equal(testDb);
       d.inMemoryOnly.should.equal(false);
 
-      async.waterfall([
-        function (cb) {
-          Persistence.ensureDirectoryExists(path.dirname(testDb), function () {
-            fs.exists(testDb, function (exists) {
-              if (exists) {
-                fs.unlink(testDb, cb);
-              } else { return cb(); }
+      async.waterfall(
+        [
+          function (cb) {
+            Persistence.ensureDirectoryExists(
+              path.dirname(testDb),
+              function () {
+                fs.exists(testDb, function (exists) {
+                  if (exists) {
+                    fs.unlink(testDb, cb);
+                  } else {
+                    return cb();
+                  }
+                });
+              }
+            );
+          },
+          function (cb) {
+            d.loadDatabase(function (err) {
+              assert.isNull(err);
+              d.getAllData().length.should.equal(0);
+              return cb();
             });
-          });
-        }
-      , function (cb) {
-          d.loadDatabase(function (err) {
-            assert.isNull(err);
-            d.getAllData().length.should.equal(0);
-            return cb();
-          });
-        }
-      ], done);
+          },
+        ],
+        done
+      );
     });
 
-    it('A throw in a callback doesnt prevent execution of next operations', function(done) {
+    it("A throw in a callback doesnt prevent execution of next operations", function (done) {
       testThrowInCallback(d, done);
     });
 
-    it('A falsy callback doesnt prevent execution of next operations', function(done) {
+    it("A falsy callback doesnt prevent execution of next operations", function (done) {
       testFalsyCallback(d, done);
     });
 
-    it('Operations are executed in the right order', function(done) {
+    it("Operations are executed in the right order", function (done) {
       testRightOrder(d, done);
     });
 
-    it('Does not starve event loop and raise warning when more than 1000 callbacks are in queue', function(done){
+    it("Does not starve event loop and raise warning when more than 1000 callbacks are in queue", function (done) {
       testEventLoopStarvation(d, done);
     });
 
-    it('Works in the right order even with no supplied callback', function(done){
+    it("Works in the right order even with no supplied callback", function (done) {
       testExecutorWorksWithoutCallback(d, done);
     });
+  }); // ==== End of 'With persistent database' ====
 
-  });   // ==== End of 'With persistent database' ====
-
-
-  describe('With non persistent database', function () {
+  describe("With non persistent database", function () {
     var d;
 
     beforeEach(function (done) {
@@ -192,22 +196,20 @@ describe('Executor', function () {
       });
     });
 
-    it('A throw in a callback doesnt prevent execution of next operations', function(done) {
+    it("A throw in a callback doesnt prevent execution of next operations", function (done) {
       testThrowInCallback(d, done);
     });
 
-    it('A falsy callback doesnt prevent execution of next operations', function(done) {
+    it("A falsy callback doesnt prevent execution of next operations", function (done) {
       testFalsyCallback(d, done);
     });
 
-    it('Operations are executed in the right order', function(done) {
+    it("Operations are executed in the right order", function (done) {
       testRightOrder(d, done);
     });
 
-    it('Works in the right order even with no supplied callback', function(done){
+    it("Works in the right order even with no supplied callback", function (done) {
       testExecutorWorksWithoutCallback(d, done);
     });
-
-  });   // ==== End of 'With non persistent database' ====
-
+  }); // ==== End of 'With non persistent database' ====
 });
